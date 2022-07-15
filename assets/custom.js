7a616b157aeba93e97a2f3be528acbaff4073415
silent0dev <ninja.silentdev@gmail.com>
2022-07-15 11:54:17 -0700

    add review section

--- assets/custom.js
+++ assets/custom.js
@@ -37,4 +37,19 @@ $(document).ready(function(){
     $('.lang-list-selecter').find('select').change(function(){
         window.location.href = '/';
     });
+
+    $('.review-items').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 798,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true
+              }
+            },
+          ]
+      });
 });
